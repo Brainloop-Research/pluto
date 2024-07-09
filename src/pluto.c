1bcca84dbaf712aaa0dfa983dfd7f8e8e6d32721
Neo <mario.sieg.64@gmail.com>
2024-07-09 09:43:46 +0200

    Update pluto.c

--- src/pluto.c
+++ src/pluto.c
@@ -331,71 +331,32 @@ bool pt_tensor_is_matmul_compatible(const struct pt_tensor_t *const a, const str
     return a->shape[1] == b->shape[0];
 }
 
-/*
-Softmax activation function
-              ∞
-            ____
-            ╲      n
-        ξ    ╲    ξ
-Ψ(ξ) = e  =  ╱    ──
-            ╱     n!
-            ‾‾‾‾
-            n = 0
-
-               ∞
-             ____
-             ╲      n
-        ξ     ╲    ξ
-Ψ'(ξ) = e  =  ╱    ──
-             ╱     n!
-             ‾‾‾‾
-             n = 0
- */
-static void PT_UNUSED pt_blas_v_softmax(const size_t n, float *o, const float *const xi) {
+static void PT_UNUSED pt_blas_v_softmax(const size_t n, float *o, const float *const x) {
     for (size_t i = 0; i < n; ++i) {
-        o[i] = expf(xi[i]);
+        o[i] = expf(x[i]);
     }
 }
 #define pt_blas_v_softmax_dv pt_blas_v_softmax // Derivative of e^x is e^x
 
-/*
-Sigmoid activation function
-
-          1
-Ψ(ξ) = ───────
-            -ξ
-       1 + e
-
-Ψ'(ξ) = Ψ(ξ) ⋅ (1 - Ψ(ξ))
-
-*/
-static void PT_UNUSED pt_blas_v_sigmoid(const size_t n, float *o, const float *const xi) {
+static void PT_UNUSED pt_blas_v_sigmoid(const size_t n, float *o, const float *const x) {
     for (size_t i = 0; i < n; ++i) {
-        o[i] = 1.0f / (1.0f + expf(-xi[i]));
+        o[i] = 1.0f / (1.0f + expf(-x[i]));
     }
 }
-static void PT_UNUSED pt_blas_v_sigmoid_dv(const size_t n, float *o, const float *const xi) {
+static void PT_UNUSED pt_blas_v_sigmoid_dv(const size_t n, float *o, const float *const x) {
     for (size_t i = 0; i < n; ++i) {
-        const float psi_of_xi = 1.0f / (1.0f + expf(-xi[i]));
-        o[i] = psi_of_xi * (1.0f - psi_of_xi);
+        const float y = 1.0f / (1.0f + expf(-x[i]));
+        o[i] = y * (1.0f - y);
     }
 }
 
-/*
-Ψ(ξ) = max(0, ξ)
-
-        ⎛0   for ξ < 0
-Ψ'(ξ) = ⎜1   for ξ > 0
-        ⎝UDF for ξ = 0 (undefined), because left and right derivatives are different but we just use 0.
-
-*/
-static void PT_UNUSED pt_blas_v_relu(const size_t n, float *o, const float *const xi) {
+static void PT_UNUSED pt_blas_v_relu(const size_t n, float *o, const float *const x) {
     for (size_t i = 0; i < n; ++i) {
-        o[i] = fmaxf(0.0f, xi[i]);
+        o[i] = fmaxf(0.0f, x[i]);
     }
 }
-static void PT_UNUSED pt_blas_v_relu_dv(const size_t n, float *o, const float *const xi) {
+static void PT_UNUSED pt_blas_v_relu_dv(const size_t n, float *o, const float *const x) {
     for (size_t i = 0; i < n; ++i) {
-        o[i] = xi[i] > 0.0f ? 1.0f : 0.0f;
+        o[i] = x[i] > 0.0f ? 1.0f : 0.0f;
     }
 }
