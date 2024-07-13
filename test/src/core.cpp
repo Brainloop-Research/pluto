336a244b252e78fc0698ac8e6e1f57550882d576
Neo <mario.sieg.64@gmail.com>
2024-07-13 06:50:34 +0200

    Update core.cpp

--- test/src/core.cpp
+++ test/src/core.cpp
@@ -17,12 +17,12 @@ GTEST_TEST(core, ctx_pool_alloc) {
 
 GTEST_TEST(core, ctx_pool_alloc_aligned) {
     context ctx {};
-    int *const x = static_cast<int *>(ctx.pool_alloc_raw_aligned(sizeof(int), 64));
+    int *x = static_cast<int *>(ctx.pool_alloc_raw_aligned(sizeof(int), 64));
     *x = 42;
     ASSERT_EQ(*x, 42);
     ASSERT_EQ(0, std::bit_cast<std::uintptr_t>(x) % 64);
     for (int i {1}; i < 1000; i <<= 1) {
-        int *const x = static_cast<int *>(ctx.pool_alloc_raw_aligned(sizeof(int), i));
+        x = static_cast<int *>(ctx.pool_alloc_raw_aligned(sizeof(int), i));
         ASSERT_EQ(0, std::bit_cast<std::uintptr_t>(x) % i);
     }
 }
