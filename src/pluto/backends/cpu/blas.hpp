0214eedf1e9e1746dab5679e9b211330b84147a6
Neo <mario.sieg.64@gmail.com>
2024-08-23 16:50:25 +0200

    BLAS explicit specialioze

--- src/pluto/backends/cpu/blas.hpp
+++ src/pluto/backends/cpu/blas.hpp
@@ -17,27 +17,58 @@ namespace pluto::backends::cpu::blas {
 
     template <typename T> requires is_dtype<T>
     extern auto PT_HOTPROC v_softmax(dim n, T* __restrict__ o, const T* __restrict__ x) noexcept -> void;
+    template <>
+    auto PT_HOTPROC v_softmax(dim n, float* __restrict__ o, const float* __restrict__ x) noexcept -> void;
+
     template <typename T> requires is_dtype<T>
     extern auto PT_HOTPROC v_sigmoid(dim n, T* __restrict__ o, const T* __restrict__ x) noexcept -> void;
+    template <>
+    auto PT_HOTPROC v_sigmoid(dim n, float* __restrict__ o, const float* __restrict__ x) noexcept -> void;
+
     template <typename T> requires is_dtype<T>
     extern auto PT_HOTPROC v_tanh(dim n, T* __restrict__ o, const T* __restrict__ x) noexcept -> void;
+    template <>
+    auto PT_HOTPROC v_tanh(dim n, float* __restrict__ o, const float* __restrict__ x) noexcept -> void;
+
     template <typename T> requires is_dtype<T>
     extern auto PT_HOTPROC v_relu(dim n, T* __restrict__ o, const T* __restrict__ x) noexcept -> void;
+    template <>
+    auto PT_HOTPROC v_relu(dim n, float* __restrict__ o, const float* __restrict__ x) noexcept -> void;
+
     template <typename T> requires is_dtype<T>
     extern auto PT_HOTPROC v_gelu(dim n, T* __restrict__ o, const T* __restrict__ x) noexcept -> void;
+    template <>
+    auto PT_HOTPROC v_gelu(dim n, float* __restrict__ o, const float* __restrict__ x) noexcept -> void;
+
     template <typename T> requires is_dtype<T>
     extern auto PT_HOTPROC v_silu(dim n, T* __restrict__ o, const T* __restrict__ x) noexcept -> void;
+    template <>
+    auto PT_HOTPROC v_silu(dim n, float* __restrict__ o, const float* __restrict__ x) noexcept -> void;
 
     template <typename T> requires is_dtype<T>
     extern auto PT_HOTPROC v_add(dim n, T* __restrict__ o, const T* __restrict__ x, const T* __restrict__ y) noexcept -> void;
+    template <>
+    auto PT_HOTPROC v_add(dim n, float* __restrict__ o, const float* __restrict__ x, const float* __restrict__ y) noexcept -> void;
+
     template <typename T> requires is_dtype<T>
     extern auto PT_HOTPROC v_sub(dim n, T* __restrict__ o, const T* __restrict__ x, const T* __restrict__ y) noexcept -> void;
+    template <>
+    auto PT_HOTPROC v_sub(dim n, float* __restrict__ o, const float* __restrict__ x, const float* __restrict__ y) noexcept -> void;
+
     template <typename T> requires is_dtype<T>
     extern auto PT_HOTPROC v_mul(dim n, T* __restrict__ o, const T* __restrict__ x, const T* __restrict__ y) noexcept -> void;
+    template <>
+    auto PT_HOTPROC v_mul(dim n, float* __restrict__ o, const float* __restrict__ x, const float* __restrict__ y) noexcept -> void;
+
     template <typename T> requires is_dtype<T>
     extern auto PT_HOTPROC v_div(dim n, T* __restrict__ o, const T* __restrict__ x, const T* __restrict__ y) noexcept -> void;
+    template <>
+    auto PT_HOTPROC v_div(dim n, float* __restrict__ o, const float* __restrict__ x, const float* __restrict__ y) noexcept -> void;
+
     template <typename T> requires is_dtype<T>
     [[nodiscard]] extern auto PT_HOTPROC v_dot(dim n, const T* __restrict__ x, const T* __restrict__ y) noexcept -> T;
+    template <>
+    [[nodiscard]] auto PT_HOTPROC v_dot(dim n, const float* __restrict__ x, const float* __restrict__ y) noexcept -> float;
 
     // ---- Tensor Operations ----
 
